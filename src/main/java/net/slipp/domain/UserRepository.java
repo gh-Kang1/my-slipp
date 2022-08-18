500bf72e44856dd70444c5d6e5d2d903104b0bdd
ghKang1 <dsds60321@gmail.com>
2022-08-18 23:17:43 +0900

    update

---
+++ src/main/java/net/slipp/domain/UserRepository.java
@@ -0,0 +1,13 @@
+package net.slipp.domain;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users,Long> {
+
+    public default Users findOne(Long id) {
+        return findById(id).orElse(null);
+    }
+}
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,26 +1,47 @@
 package net.slipp.web;
 
+import net.slipp.domain.UserRepository;
+import net.slipp.domain.Users;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    private List<User> users = new ArrayList<User>();
+    private List<Users> users = new ArrayList<Users>();
 
-    @PostMapping("/create")
-    public String create(User user,Model model) {
-        users.add(user);
-        return "redirect:/list";
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping
+    public String create(Users users, Model model) {
+        userRepository.save(users);
+        return "redirect:/users";
     }
 
-    @GetMapping("/list")
+    @GetMapping
     public String list(Model model) {
-        model.addAttribute("users",users);
-        return "list";
+        model.addAttribute("users",userRepository.findAll());
+        return "/user/list";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "/user/form";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model) {
+        model.addAttribute("user", userRepository.findOne(id));
+        return "/user/updateForm";
     }
 }
